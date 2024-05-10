00df8f01b2f0e41d5bbb389125cbaccb0df2a7c3
BenchPress200 <jms0538@naver.com>
2024-05-10 22:14:57 +0900

    feat: add event to navigate from welcome page to sign-in page

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
-// localhost:3000 이동시 public/index.html이 뿌려짐
-// 그 다음 index.js가 진입점으로서 실행됨
-// index.html에서 Root가져옴
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
-// 루트에다가 App 컴포넌트 뿌림, 현재 앱은 라우팅 컴포넌트
 root.render(
   <React.StrictMode>
     <App/> 
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -5,29 +5,24 @@ import HelperText from "../components/HelperText.js";
 
 import "../styles/pages/sign-in.css";
 
+
+
 const SignIn = () => {
     return (
         <>
             <Header backBtnVisibility="hidden" profileImageVisibility="hidden"></Header>
 
-
             <form id="sign-in-form" action="/posts" method="get">
-                <div id="sign-in-form-title">로그인</div>
-        
+                <div id="sign-in-form-title">로그인</div>        
                 <div id="sign-in-input-box">
-
                     <TextInput type="email"></TextInput>
                     <TextInput type="password"></TextInput>
                     <HelperText></HelperText>
-
                 </div>
-
                 <button id="sign-in-btn" type="submit">로그인</button>
             </form>
 
-
             <button id="move-sign-up-btn" onclick="location.href='/users/sign-up'">회원가입</button>
-
         </>
     );
   }
